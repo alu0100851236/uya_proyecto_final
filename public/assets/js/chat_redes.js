5f28768d40e63f6da0dd9c1e999414b9c1bd0797
alu0100851236 <alu0100851236@ull.edu.es>
2017-06-20 18:39:25 +0100

    Creados los js para los foros de las asignaturas y el login inicial

---
+++ public/assets/js/chat_redes.js
@@ -0,0 +1,23 @@
+var myDataRef = new Firebase('https://redes-8ba2f.firebaseio.com');
+
+$('#mensajeInput').keypress(function (e) {
+
+    if (e.keyCode == 13) {
+    var name = $('#nombreInput').val();
+    var text = $('#mensajeInput').val();
+    myDataRef.push({name: name, text: text});
+    $('#mensajeInput').val('');
+    }
+
+});
+
+myDataRef.on('child_added', function(snapshot) {
+    var message = snapshot.val();
+    displayChatMessage(message.name, message.text);
+});
+
+function displayChatMessage(name, text) {
+  console.log("name: " + text)
+    $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#mensaje'));
+    $('#mensaje')[0].scrollTop = $('#mensaje')[0].scrollHeight;
+};
