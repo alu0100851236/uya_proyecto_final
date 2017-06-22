7b681b0d5c94182d652921d33249da7f4de109d6
alu0100851236 <alu0100851236@ull.edu.es>
2017-06-22 18:58:21 +0100

    limpieza del server.js

--- server.js
+++ server.js
@@ -41,23 +41,6 @@ app.get('/', function (req, res) {
    res.sendFile( __dirname + '/public/login.html');
 });
 
-// app.get('/index1.html', function (req, res) {
-//    res.sendFile( __dirname + "/" + "index1.html" );
-// })
-
-// app.get('/base_de_datos.html', function (req, res) {
-//    res.sendFile( __dirname + "/" + "base_de_datos.html" );
-// })
-// app.get('/mapa_web.html', function (req, res) {
-//    res.sendFile( __dirname + "/" + "mapa_web.html" );
-// })
-// app.get('/redes_de_computadores.html', function (req, res) {
-//    res.sendFile( __dirname + "/" + "redes_de_computadores.html" );
-// })
-// app.get('/uya.html', function (req, res) {
-//    res.sendFile( __dirname + "/" + "uya.html" );
-// })
-
 // Definimos una ruta para hacer el login de usuarios
 app.post('/login', function(req, res) {
   sqlUsername = db.prepare("SELECT username FROM t_usuarios WHERE username='" + req.body.username + "' and password='" + req.body.password + "'");
