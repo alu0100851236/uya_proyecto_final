51ea93d5f57bd93ca87ad92d77d39492e53005d8
charly-poket <alu0100844665@ull.edu.es>
2017-09-08 04:34:13 +0100

    Remate_final_proyecto
    Añadidos cambios solicitados por los usuarios …

--- public/assets/js/pages/dashboard.js
+++ public/assets/js/pages/dashboard.js
@@ -1,6 +1,5 @@
 $( document ).ready(function() {
 
-    setTimeout(function(){ Materialize.toast('Bienvenido a Collaborate Classroom !', 4000) }, 4000);
     setTimeout(function(){ Materialize.toast('You have 4 new notifications', 4000) }, 11000);
 
 
